5133b04a97a1bc9d3cb50e04972a32aefe2ab8bb
kawwag11-web <kawwag11@gmail.com>
2026-06-25 16:23:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, //ADD YOUR SSION ID
-    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'UUd11ALb#NZAsHXgUzS1TXrhhUV1kczAqAvQJqDBeMvJb82z3suQ' : process.env.SESSION_ID, //ADD YOUR SSION ID
+    GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'kawwag11-web' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
 
