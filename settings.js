d0f7a2d269376edd56bee0f5c03379f67815895f
kawwag11-web <kawwag11@gmail.com>
2026-06-25 17:38:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'UUd11ALb#NZAsHXgUzS1TXrhhUV1kczAqAvQJqDBeMvJb82z3suQ' : process.env.SESSION_ID, //ADD YOUR SSION ID
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'pFEQhbbQ#xYjfIsd27yLVvSi9okDZz4H0Tf4nQOaLAk0Spdv6mcY' : process.env.SESSION_ID, //ADD YOUR SSION ID
     GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'kawwag11-web' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
 
 /*
